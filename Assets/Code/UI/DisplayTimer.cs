3ba13289b19a326c13f5f7fa7246fc582ffbf233
Ian <ian.dobrincic@gmail.com>
2026-07-23 14:42:48 +0200

    bug fixes & added prefabs

--- Assets/Code/UI/DisplayTimer.cs
+++ Assets/Code/UI/DisplayTimer.cs
@@ -15,7 +15,7 @@ public class DisplayTimer : MonoBehaviour
     {
         if (!TimerManager.instance) return;
         var minutes = Mathf.Floor(TimerManager.instance.Timer / 60);
-        var seconds = TimerManager.instance.Timer % 60;
+        var seconds = Mathf.Floor(TimerManager.instance.Timer % 60);
         // var milliseconds = TimerManager.instance.Timer / 100; // idek, bro
         _timerText.text = $"{minutes:00}:{seconds:00}";
     }
