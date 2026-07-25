083f58b1e152ec634a98a4ecfa86c9ad8ef49345
Ian <ian.dobrincic@gmail.com>
2026-07-25 12:25:34 +0200

    level 1 finished i think

--- Assets/Code/NPCs/NPCController.cs
+++ Assets/Code/NPCs/NPCController.cs
@@ -13,13 +13,17 @@ public class NPCController : MonoBehaviour
     private float movementCooldownTimerBuffer;
     private float movementTimerBuffer;
     private Vector2 randomDirection;
-    private Animator animator;
-    private Rigidbody2D rb;
+    private Animator _animator;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _spriteRenderer;
+    private ParticleSystem _particleSystem;
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        TryGetComponent(out _particleSystem);
     }
 
     private void Start()
@@ -31,13 +35,15 @@ public class NPCController : MonoBehaviour
     
     private void Update()
     {
-        rb.linearVelocity = Vector2.zero;
+        _rb.linearVelocity = Vector2.zero;
         
         movementCooldownTimerBuffer -= Time.deltaTime;  // Decrease cooldown timer
         if (movementCooldownTimerBuffer > 0) return;    // If the timer is not 0, return
         
         movementTimerBuffer -= Time.deltaTime;  // Decrease the movement timer
-        animator.SetBool("isWalking", true);
+        _animator.SetBool("isWalking", true);
+        _spriteRenderer.flipX = randomDirection.x > 0;
+        if (_particleSystem) _particleSystem.transform.rotation = Quaternion.Euler(0f, Mathf.Ceil(randomDirection.x) * 180, 0);
         if (movementTimerBuffer > 0) return;    // If the timer is above 0, return and keep moving
         Initialize();    // Once it's done, reset both timers
     }
@@ -45,12 +51,12 @@ public class NPCController : MonoBehaviour
     private void FixedUpdate()
     {
         if (movementCooldownTimerBuffer > 0) return;
-        rb.MovePosition((Vector2)transform.position + randomDirection * (movementSpeed * Time.fixedDeltaTime));
+        _rb.MovePosition((Vector2)transform.position + randomDirection * (movementSpeed * Time.fixedDeltaTime));
     }
 
     private void Initialize()
     {         
-        animator.SetBool("isWalking", false);
+        _animator.SetBool("isWalking", false);
         
         randomDirection = GetRandomDirection(); // Set the random direction beforehand, so that it doesn't constantly change later
         var randomCooldownBonus = Random.Range(randomAddedRange.x, randomAddedRange.y); // Add Remove anywhere from range
@@ -66,6 +72,6 @@ public class NPCController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        movementTimerBuffer = 0;
+        movementTimerBuffer = -1;
     }
 }
